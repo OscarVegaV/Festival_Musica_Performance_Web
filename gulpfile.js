263be697dd311555f813b2711022300b0f60fee8
Oscar Vega <os_ar1@hotmail.com>
2024-10-22 15:27:28 -0600

    Add variables for fonts, update gulpfile, and enhance app styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
-import { src, dest, watch } from 'gulp' // 'src' defines the source files, 'dest' specifies where the output files will be saved
-import * as darkSass from 'sass' // Import all modules from 'sass' (Dart Sass)
-import gulpSass from 'gulp-sass' // Import the Gulp plugin to compile Sass files
+import { src, dest, watch } from 'gulp'; // 'src' defines the source files, 'dest' specifies the output
+import * as dartSass from 'sass'; // Import all modules from 'sass' (Dart Sass)
+import gulpSass from 'gulp-sass'; // Import Gulp plugin to compile Sass files
 
-const sass = gulpSass(darkSass) // Use 'gulp-sass' and pass the Dart Sass implementation
+const sass = gulpSass(dartSass); // Use 'gulp-sass' and pass Dart Sass
 
-// Compiling Sass files from the 'src/scss' directory and outputting the result as CSS to the 'build/css' directory
-
+// Compile Sass files from 'src/scss' and output them to 'build/css'
 export function css(done) {
-  src('src/scss/app.scss')//where locate is
-    .pipe( sass())// copiled sass
-    .pipe( dest('build/css'))// Save the compiled CSS file in the 'build/css' folder
-    
-  done() // Callback function to signal Gulp that the task is complete
-
+  src('src/scss/app.scss') // Source location of Sass file
+    .pipe(sass().on('error', sass.logError)) // Compile Sass and log errors
+    .pipe(dest('build/css')); // Output to 'build/css'
+  
+  done(); // Signal that the task is done
 }
 
-// this is useful for 
+// Watch all Sass files in 'src/scss' for changes and recompile
 export function dev() {
-  watch('src/scss/app.scss', css)
+  watch('src/scss/**/*.scss', css); // Watch all .scss files
 }
