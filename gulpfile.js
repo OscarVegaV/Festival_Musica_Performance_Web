0fc2cf49fbd7447edbcd64f0941eaa5981ed1faa
Oscar Vega <os_ar1@hotmail.com>
2024-10-21 21:31:30 -0600

    Compiling Sass files from the 'src/scss' directory and outputting the result as CSS to the 'build/css' directory | Enable watch mode with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-import { src, dest } from 'gulp' // 'src' defines the source files, 'dest' specifies where the output files will be saved
-import * as darrSass from 'sass' // Import all modules from 'sass' (Dart Sass)
+import { src, dest, watch } from 'gulp' // 'src' defines the source files, 'dest' specifies where the output files will be saved
+import * as darkSass from 'sass' // Import all modules from 'sass' (Dart Sass)
 import gulpSass from 'gulp-sass' // Import the Gulp plugin to compile Sass files
 
-const sass = gulpSass(darrSass) // Use 'gulp-sass' and pass the Dart Sass implementation
+const sass = gulpSass(darkSass) // Use 'gulp-sass' and pass the Dart Sass implementation
 
 // Compiling Sass files from the 'src/scss' directory and outputting the result as CSS to the 'build/css' directory
 
@@ -13,4 +13,9 @@ export function css(done) {
     
   done() // Callback function to signal Gulp that the task is complete
 
+}
+
+// this is useful for 
+export function dev() {
+  watch('src/scss/app.scss', css)
 }
