1aa94cd845574286021c7e2684e5542eacb4f51e
Oscar Vega <os_ar1@hotmail.com>
2024-10-21 20:36:31 -0600

    Normalizing line endings

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,14 @@
-export function gulpGreetig(done) {
-  console.log("hola desde gulpfile.js");
-  done()// use this callback always
-};
+import { src, dest } from 'gulp' // 'src' defines the source files, 'dest' specifies where the output files will be saved
+import * as darrSass from 'sass' // Import all modules from 'sass' (Dart Sass)
+import gulpSass from 'gulp-sass' // Import the Gulp plugin to compile Sass files
+
+const sass = gulpSass(darrSass) // Use 'gulp-sass' and pass the Dart Sass implementation
+
+export function css(done) {
+  src('src/scss/app.scss')//where locate is
+    .pipe( sass())// copiled sass
+    .pipe( dest('build/css'))// Save the compiled CSS file in the 'build/css' folder
+    
+  done() // Callback function to signal Gulp that the task is complete
+
+}
