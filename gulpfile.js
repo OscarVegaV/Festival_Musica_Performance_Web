7c1c11c453238ed9f3252bae40ce0cea30337cf0
Oscar Vega <os_ar1@hotmail.com>
2024-10-25 13:51:47 -0600

    Incorporating JS into the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-import { src, dest, watch } from 'gulp'; // 'src' defines the source files, 'dest' specifies the output
+import { src, dest, watch, series } from 'gulp'; // 'src' defines the source files, 'dest' specifies the output 'series' take diferents exports 
 import * as dartSass from 'sass'; // Import all modules from 'sass' (Dart Sass)
 import gulpSass from 'gulp-sass'; // Import Gulp plugin to compile Sass files
 
 const sass = gulpSass(dartSass); // Use 'gulp-sass' and pass Dart Sass
+
+// Define a task to copy JavaScript files from 'src/js' to 'build/js'
+export function js(done) {
+
+  src( 'src/js/app.js') // Define the source location of the JavaScript file
+    .pipe ( dest('build/js')) // Output the JavaScript file to 'build/js'
+
+  done(); // Signal that the task is complete
+}
 
 // Compile Sass files from 'src/scss' and output them to 'build/css'
 export function css(done) {
@@ -16,4 +25,8 @@ export function css(done) {
 // Watch all Sass files in 'src/scss' for changes and recompile them when modified
 export function dev() {
   watch('src/scss/**/*.scss', css); // Monitor all .scss files in the src/scss directory
+  watch('src/js/**/*.js', js); // Monitor all .scss files in the src/scss directory
 }
+
+// Define the default task to run the JavaScript and Sass tasks, and start the watcher
+export default series( js, css, dev);// Initialize these tasks in series
