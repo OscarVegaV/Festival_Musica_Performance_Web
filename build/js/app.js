c4765f797b022a8418ea179eb45a308e567a13cd
Oscar Vega <os_ar1@hotmail.com>
2024-10-25 14:21:31 -0600

    Added dynamic image gallery with JavaScript

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,22 @@
-//alert('desde app.js');
+//
+
+document.addEventListener('DOMContentLoaded', function () {
+  makeGalery()
+});
+
+function makeGalery() {
+
+  const amount_img = 16;
+  const gelery = document.querySelector('.img-galery')
+
+  for (let img = 1; img <= amount_img; img++) {
+
+    const image = document.createElement('IMG');
+
+    image.src = `src/img/gallery/full/${img}.jpg`
+    image.alt = 'Image Galery'
+    
+    galery.appendChild(image)   
+
+  }
+};
