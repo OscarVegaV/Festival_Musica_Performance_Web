075d1fc116304d8c3cb786a27f73cc08b619c357
Oscar Vega <os_ar1@hotmail.com>
2024-10-28 20:26:28 -0600

    Completing Modal functionality

--- src/js/app.js
+++ src/js/app.js
@@ -27,23 +27,49 @@ function makeGalery() {
 
 // ** Function to display the clicked image in a modal **
 function displayImg(img) {
+
+  //Creating Image for modal
+  const image = document.createElement('IMG'); // Create an IMG element
+  image.src = `src/img/gallery/full/${img}.jpg`; // Set the source path for each image
+  image.alt = 'Image Gallery'; // Alt text for accessibility
+  
   
   // Creating Modal
   const modal = document.createElement('DIV');
-modal.classList.add('modal');// Add the modal class for styling
-modal.onclick = killModal; // Set up the click event to close the modal
+  modal.classList.add('modal');// Add the modal class for styling
+  modal.onclick = killModal; // Set up the click event to close the modal
+
+   //button
+   const killModalButton = document.createElement('BUTTON')
+   killModalButton.textContent = 'X'; // Add text 'X' for closing the modal
+   killModalButton.classList.add('button-kill') // Add class for button styling
+   killModalButton.onclick = killModalButton; // Set up button click event to close modal
+
+
+  modal.appendChild(image); // Add image to the modal
+  modal.appendChild(killModalButton); // Add close button to the modal
+
 
 
   // Add modal to the DOM
-  const body = document.querySelector('body');
-//
-body.appendChild(modal)
+  const body = document.querySelector('body');  
+  body.classList.add('overflow-hidden');// Prevent background scrolling
+  body.appendChild(modal);
+  
 }
 
 // Function to remove the modal from the DOM
 function killModal() {
   
   const modal = document.querySelector('.modal');// Select the modal element
+
+  modal.classList.add('fade-out'); // Add fade-out animation class
   //
-  modal?.remove();// Remove the modal if it exists
+  setTimeout(() => {
+    
+    modal?.remove();// Remove the modal if it exists
+
+    const body = document.querySelector('body');    
+    body.classList.remove('overflow-hidden'); // Re-enable background scrolling
+  }, 500);
 }
